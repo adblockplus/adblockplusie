afc0c74272c20e19186a322940920a94b749ba0d
shoniko <none@none>
2011-05-11 17:12:50 +0000

    personal filter loading commented out.

--- Shared/PluginFilter.cpp
+++ Shared/PluginFilter.cpp
@@ -1399,10 +1399,10 @@ bool CPluginFilter::ReadFilter(const CString& filename, const CString& downloadP
 						// Element hiding not supported yet
 						if (filterType == CFilter::filterTypeElementHide)
 						{ 
-							if ((filter.Find('[') < 0) && (filter.Find('^') < 0))
-							{
+//							if ((filter.Find('[') < 0) && (filter.Find('^') < 0))
+//							{
 								AddFilterElementHide(filter, filename);
-							}
+//							}
 						}
 						else if (filterType != CFilter::filterTypeUnknown)
 						{
@@ -1458,7 +1458,7 @@ void CPluginFilter::ParseFilters(const TFilterFileList& list)
     }
 
 #ifdef PERSONAL_FILTER_FILE
-    ReadFilter(PERSONAL_FILTER_FILE);
+//    ReadFilter(PERSONAL_FILTER_FILE);
 #endif
 
 #ifdef ENABLE_DEBUG_SELFTEST
