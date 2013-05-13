b9166073683fc786ad3febf1fdfee7b4300e1638
Wladimir Palant <trev@adblockplus.org>
2013-05-13 11:50:04 +0200

    Removed Avast files from the repository, moved these to their own repository

--- Shared/BuildVariant.h
+++ Shared/BuildVariant.h
@@ -1,7 +1,2 @@
-#ifdef ABP
 #include "..\Adblocker\AdblockPlus.h"
- #include "..\AdBlocker\Version.h"
-#else if AVAST_ABP
-  #include "..\Avast\Avast.h"
-  #include "..\Avast\Version.h"
-#endif
+#include "..\AdBlocker\Version.h"
