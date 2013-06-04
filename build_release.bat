2d5b1e315e08289920f23044ff37ca65c8b9dd23
Wladimir Palant <trev@adblockplus.org>
2013-06-04 14:55:26 +0200

    x64 libadblockplus build is no longer required for the release

--- build_release.bat
+++ build_release.bat
@@ -5,7 +5,6 @@ if %1.==. goto NoKey
 pushd %~dp0
 call libadblockplus\createsolution.bat
 msbuild libadblockplus\build\ia32\libadblockplus.sln /p:Configuration=Release
-msbuild libadblockplus\build\x64\libadblockplus.sln /p:Configuration=Release
 msbuild AdblockPlus.sln "/p:Configuration=Release Production" /p:Platform=Win32
 msbuild AdblockPlus.sln "/p:Configuration=Release Production" /p:Platform=x64
 signtool.exe sign /v /d "Adblock Plus" /du "http://adblockplus.org/" /f %1 /tr "http://www.startssl.com/timestamp" "build\ia32\Release Production\AdblockPlus.dll"
