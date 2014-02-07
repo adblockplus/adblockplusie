3d509c0e8bdeab067f6f400f90e619495d790d7e
Eric Hughes <eric@adblockplus.org>
2014-02-06 17:53:35 -0700

    Added unsigned string comparison function and a test using it to find processes. Converted template arguments in Process_List to use function classes.
    --HG--
    branch : new-installer

--- installer/src/installer-lib/process.cpp
+++ installer/src/installer-lib/process.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <wctype.h>
 
 #include "process.h"
 
@@ -14,4 +15,34 @@ Windows_Handle::Windows_Handle( HANDLE h )
 Windows_Handle::~Windows_Handle()
 {
   CloseHandle( handle ) ;
+}
+
+
+int wcsncmpi( const wchar_t * s1, const wchar_t * s2, unsigned int n )
+{
+  // Note: Equality of character sequences is case-insensitive in all predicates below.
+  // Loop invariant: s1[0..j) == s2[0..j)
+  for ( unsigned int j = 0 ; j < n ; ++j )
+  {
+    wchar_t c1 = towupper( *s1++ ) ;
+    wchar_t c2 = towupper( *s2++ ) ;
+    if ( c1 != c2 )
+    {
+      // Map to -1/+1 because c2 - c1 may not fit into an 'int'.
+      return ( c1 < c2 ) ? -1 : 1 ;
+    }
+    else
+    {
+      if ( c1 == L'\0' )
+      {
+	// Assert length( s1 ) == length( s2 ) == j
+	// Assert strings are equal at length < n
+	return 0 ;
+      }
+    }
+  }
+  // Assert j == n
+  // Assert s1[0..n) == s2[0..n)
+  // The semantics of n-compare ignore everything after the first 'n' characters.
+  return 0 ;
 }
--- installer/src/installer-lib/test/process_test.cpp
+++ installer/src/installer-lib/test/process_test.cpp
@@ -1,66 +1,91 @@
 #include <gtest/gtest.h>
-
 #include "../process.h"
+#include <functional>
 
 
 /**
  * A promiscuous filter admits everything.
  */
-bool trivial_true( PROCESSENTRY32W & )
+struct always_true
+  : public std::unary_function< PROCESSENTRY32W, bool >
 {
-  return true;
-}
+  bool operator()( const PROCESSENTRY32W & ) { return true ; } ;
+} ;
 
-/*
- * Converter that copies everything, by reference.
- */
-PROCESSENTRY32W & copy_all( PROCESSENTRY32W & process )
+struct copy_all
+  : public std::unary_function< PROCESSENTRY32W, PROCESSENTRY32W >
 {
-  return process ;
-}
+  PROCESSENTRY32W operator()( const PROCESSENTRY32W & process ) { return process ; }
+} ;
 
 /**
  * Construction test ensures that we don't throw and that at least one process shows up.
  */
 TEST( Process_List_Test, construct )
 {
-  Process_List< PROCESSENTRY32W > pl( trivial_true, copy_all );
+  Process_List< PROCESSENTRY32W, always_true, copy_all > pl ;
   ASSERT_GE( pl.v.size(), 1u );
 }
 
 /**
  * Filter by a fixed name.
  */
-bool our_process_by_name( PROCESSENTRY32W & process )
+struct our_process_by_name
+  : std::unary_function< PROCESSENTRY32W, bool >
 {
-  return 0 == wcscmp( process.szExeFile, L"installer-ca-tests.exe" );
-}
+  bool operator()( const PROCESSENTRY32W & process ) { return 0 == wcscmp( process.szExeFile, L"installer-ca-tests.exe" ); } ;
+};
 
 /**
  * The only process we are really guaranteed to have is this test process itself.
  */
 TEST( Process_List_Test, find_our_process )
 {
-  Process_List< PROCESSENTRY32W > pl( our_process_by_name, copy_all );
+  Process_List< PROCESSENTRY32W, our_process_by_name, copy_all > pl ;
   unsigned int size( pl.v.size() );
-  EXPECT_EQ( size, 1u );    // Please, don't run multiple test executables simultaneously
-  ASSERT_GE( size, 1u );
+  EXPECT_EQ( 1u, size );    // Please, don't run multiple test executables simultaneously
+  ASSERT_GE( 1u, size );
 }
 
-/*
- * Converter that copies only the PID, by value.
- */
-DWORD copy_PID( PROCESSENTRY32W & process )
+
+struct our_process_by_name_CI
+  : std::unary_function< PROCESSENTRY32W, bool >
 {
-  return process.th32ProcessID ;
+  bool operator()( const PROCESSENTRY32W & process ) 
+  {
+    static const wchar_t s2[] = L"Installer-CA-Tests.exe";
+    return 0 == wcsncmpi( process.szExeFile, s2, sizeof( s2 ) / sizeof( wchar_t ) ) ;
+  } ;
+} ;
+
+/**
+ * The only process we are really guaranteed to have is this test process itself.
+ */
+TEST( Process_List_Test, find_our_process_CI )
+{
+  Process_List< PROCESSENTRY32W, our_process_by_name_CI, copy_all > pl ;
+  unsigned int size( pl.v.size() );
+  EXPECT_EQ( 1u, size );    // Please, don't run multiple test executables simultaneously
+  ASSERT_GE( 1u, size );
 }
+
+
+/*
+ * Extractor that copies only the PID.
+ */
+struct copy_PID
+  : public std::unary_function< PROCESSENTRY32W, DWORD >
+{
+  DWORD operator()( const PROCESSENTRY32W & process ) { return process.th32ProcessID ; }
+} ;
+
 
 /**
  * Locate the PID of our process.
  */
 TEST( Process_List_Test, find_our_PID )
 {
-  Process_List< DWORD > pl( our_process_by_name, copy_PID );
+  Process_List< DWORD, our_process_by_name, copy_PID > pl ;
   unsigned int size( pl.v.size() );
   EXPECT_EQ( size, 1u );    // Please, don't run multiple test executables simultaneously
   ASSERT_GE( size, 1u );
