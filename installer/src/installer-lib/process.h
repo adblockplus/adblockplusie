a742d2e1210eaa9aed63f122ed5febaa6831dd7d
Eric Hughes <eric@adblockplus.org>
2014-02-06 17:53:35 -0700

    Added unsigned string comparison function and a test using it to find processes. Converted template arguments in Process_List to use function classes.
    --HG--
    branch : new-installer
    extra : amend_source : 1aa942dea5400dcafd74075e61b257de007b00fd

--- installer/src/installer-lib/process.h
+++ installer/src/installer-lib/process.h
@@ -47,11 +47,15 @@ private:
 };
 
 /**
- * \tparam Predicate A unary predicate type, either function pointer or function object. Ordinarily inferred from the parameter.
+ * \tparam T The type into which a PROCESSENTRY32W struture is extracted.
+ * \tparam Admittance A unary predicate function class that determines what's included
  */
-template< class T >
+template< class T, class Admittance, class Extractor >
 class Process_List
 {
+private:
+  Admittance admit;
+  Extractor extract;
 public:
   /**
    * \tparam Predicate Function pointer or function object. Generally inferred from the argument.
@@ -60,9 +64,43 @@ public:
    *	The use of this predicate is analogous to that in std::copy_if.
    * \tparam Converter Function pointer or function object. Generally inferred from the argument.
    * \param convert A conversion function that takes a PROCESSENTRY32W as input argument and returns an element of type T.
+   *
+   * \par Implementation
+   *
+   * CreateToolhelp32Snapshot function http://msdn.microsoft.com/en-us/library/windows/desktop/ms682489%28v=vs.85%29.aspx
+   * Process32First function http://msdn.microsoft.com/en-us/library/windows/desktop/ms684834%28v=vs.85%29.aspx
+   * Process32Next function http://msdn.microsoft.com/en-us/library/windows/desktop/ms684836%28v=vs.85%29.aspx
+   * PROCESSENTRY32 structure http://msdn.microsoft.com/en-us/library/windows/desktop/ms684839%28v=vs.85%29.aspx
    */
-  template< class Predicate, class Converter >
-  Process_List( Predicate admit, Converter convert );
+  Process_List()
+  {
+    /*
+     * Take a snapshot only of all processes on the system, and not all the other data available through the 
+     * CreateToolhelp32Snapshot. The second argument is ignored when the only flag is TH32CS_SNAPPROCESS.
+     */
+    Windows_Handle handle( ::CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 ) );
+    /*
+     * Initialization of the size member of the process structure is required for Process32FirstW to succeed.
+     */
+    PROCESSENTRY32W process;
+    process.dwSize = sizeof( PROCESSENTRY32W );
+    /*
+     * Process32FirstW and Process32NextW iterate through the process snapshot.
+     */
+    BOOL have_process = ::Process32FirstW( handle, & process );
+    while ( have_process )
+    {
+      if ( admit( process ) )
+      {
+	/*
+	 * We don't have C++11 emplace_back, which can construct the element in place.
+	 * Instead, we copy the return value of the converter.
+	 */
+	v.push_back( extract( process ) );
+      }
+      have_process = ::Process32NextW( handle, & process );
+    }
+  };
 
   /**
    */
@@ -75,40 +113,8 @@ public:
   std::vector< T > v;
 };
 
-/**
- * \par Implementation
- *
- * CreateToolhelp32Snapshot function http://msdn.microsoft.com/en-us/library/windows/desktop/ms682489%28v=vs.85%29.aspx
- * Process32First function http://msdn.microsoft.com/en-us/library/windows/desktop/ms684834%28v=vs.85%29.aspx
- * Process32Next function http://msdn.microsoft.com/en-us/library/windows/desktop/ms684836%28v=vs.85%29.aspx
- * PROCESSENTRY32 structure http://msdn.microsoft.com/en-us/library/windows/desktop/ms684839%28v=vs.85%29.aspx
- */
-template< class T > template< class Predicate, class Converter > 
-Process_List< T >::Process_List( Predicate admit, Converter convert )
-{
-  /*
-   * Take a snapshot only of all processes on the system, and not all the other data available through the CreateToolhelp32Snapshot.
-   * Second argument is ignored for flag TH32CS_SNAPPROCESS.
-   */
-  Windows_Handle handle( ::CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 ) );
-  // Size initialization required for Process32FirstW to succeed.
-  PROCESSENTRY32W process;
-  process.dwSize = sizeof( PROCESSENTRY32W );
-  /*
-   * Process32FirstW and Process32NextW iterate through the process snapshot.
-   */
-  BOOL have_process = ::Process32FirstW( handle, & process );
-  while ( have_process )
-  {
-    if ( admit( process ) )
-    {
-      /*
-       * We don't have C++11 emplace_back, which can construct the element in place.
-       * Instead, we copy the return value of the converter.
-       */
-      v.push_back( convert( process ) );
-    }
-    have_process = ::Process32NextW( handle, & process );
-  }
-}
 
+/**
+ * Case-insensitive wide-character C-style string comparison, fixed-length
+ */
+int wcsncmpi( const wchar_t * a, const wchar_t * b, unsigned int length ) ;
