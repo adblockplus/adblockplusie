291840beec660da08abe38763869fc1e0cba930a
Eric Hughes <eric@adblockplus.org>
2014-03-23 22:30:55 -0600

    installer-v2 code
    --HG--
    branch : installer-v2
    rename : installer/bho_registry_value.wxi => installer/src/msi/bho_registry_value.wxi
    rename : installer/custom_WixUI_InstallDir.wxs => installer/src/msi/custom_WixUI_InstallDir.wxs
    rename : installer/dll_class.wxi => installer/src/msi/dll_class.wxi
    rename : installer/abp-64.png => installer/src/setup-exe/abp-64.png
    rename : installer/bootstrap-theme.wxl => installer/src/setup-exe/bootstrap-theme.wxl
    rename : installer/bootstrap-theme.xml => installer/src/setup-exe/bootstrap-theme.xml
    rename : installer/setup.wxs => installer/src/setup-exe/setup.wxs

---
+++ installer/src/installer-lib/interaction.cpp
@@ -0,0 +1,29 @@
+/**
+ * \file interaction.cpp Implementations of user interaction classes.
+ */
+
+#include "interaction.h"
+
+/*
+ * The two constructors are identical except for the type of argument 'message'
+ * They rely on overloads of the Message constructor
+ */
+Installer_Message_Box::Installer_Message_Box( 
+  std::wstring message, 
+  box_type box,
+  buttonset_type buttonset, 
+  default_button_type default_button,
+  icon_type icon
+)
+  : Message( message, INSTALLMESSAGE( box | buttonset | default_button | icon ) )
+{}
+
+Installer_Message_Box::Installer_Message_Box( 
+  std::string message, 
+  box_type box, 
+  buttonset_type buttonset, 
+  default_button_type default_button,
+  icon_type icon
+)
+  : Message( message, INSTALLMESSAGE( box | buttonset | default_button | icon ) )
+{}
---
+++ installer/src/installer-lib/test/custom-action-fail.cpp
@@ -0,0 +1,29 @@
+/**
+ * \file close_application.cpp
+ */
+
+#include "session.h"
+
+//-------------------------------------------------------
+// Fail
+//-------------------------------------------------------
+/**
+ * A custom action that always and immediately fails.
+ * Use during testing to ensure that the installer terminates.
+ *
+ * \param[in] session_handle
+ *     Windows installer session handle
+ *
+ * \return 
+ *    An integer interpreted as a custom action return value.
+ *   
+ * \sa
+ *   - MSDN [Custom Action Return Values](http://msdn.microsoft.com/en-us/library/aa368072%28v=vs.85%29.aspx)
+ */
+extern "C" UINT __stdcall 
+fail( MSIHANDLE session_handle )
+{
+  // Instantiate the session object in order to get begin/end log entries.
+  Immediate_Session session( session_handle, "fail" ) ;
+  return ERROR_INSTALL_FAILURE ;
+}
