8168b39417c193d94e1d33bda80aaa58efebac30
Eric Hughes <eric@adblockplus.org>
2014-08-01 07:42:13 -0600

    Issue #276 - std::wstring version of UnescapeUrl
    Added a std::wstring version of UnescapeUrl as a standalone function. The
    existing version remains in place; refactoring all calls to this function
    simultaneously causes too many problems.

--- src/plugin/PluginClientBase.cpp
+++ src/plugin/PluginClientBase.cpp
@@ -57,6 +57,33 @@ CString& CPluginClientBase::UnescapeUrl(CString& url)
   return url;
 }
 
+void UnescapeUrl(std::wstring& url)
+{
+  try
+  {
+    DWORD result_length = INTERNET_MAX_URL_LENGTH;
+    std::unique_ptr<wchar_t[]> result(new wchar_t[result_length]);
+    HRESULT hr = UrlUnescapeW(const_cast<wchar_t*>(url.c_str()), result.get(), &result_length, 0);
+    if (hr == S_OK)
+    {
+      url = std::wstring(result.get(), result_length);
+    }
+    /*
+     * Do nothing. This masks error return values from UrlUnescape without logging the error.
+     */
+  }
+  catch(std::bad_alloc e)
+  {
+    /*
+     * When the code has a systematic way of handling bad_alloc, we'll rethrow (probably).
+     * Until then, we mask the exception and make no modification.
+     */
+  }
+  catch(...)
+  {
+    // no modification if any other exception
+  }
+}
 
 void CPluginClientBase::LogPluginError(DWORD errorCode, int errorId, int errorSubid, const CString& description, bool isAsync, DWORD dwProcessId, DWORD dwThreadId)
 {
