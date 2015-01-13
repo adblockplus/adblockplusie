331c7646bb9ccfa5f8450820a93d37b1deee45f5
Oleksandr Paraska <oleksandr@adblockplus.org>
2015-01-13 21:51:45 +0200

    Noissue - Add GPL headers

--- src/plugin/Console.h
+++ src/plugin/Console.h
@@ -1,4 +1,21 @@
 /*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-2015 Eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
   Macros CONSOLE, CONSOLE_WARN, CONSOLE_ERROR are used for Console output
   CONSOLE_WAIT waits for user to hit Enter
   In order to see Console output macro USE_CONSOLE should be defined before Console.h is included.
