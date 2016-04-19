b6d0cad28831e4e358a69b60fa118f6db66cc149
Sebastian Noack <sebastian@adblockplus.org>
2016-04-19 12:32:54 +0200

    Issue 3952 - Fix whitespaces for compliance with PEP-8

--- build_release.py
+++ build_release.py
@@ -1,70 +1,77 @@
 #!/usr/bin/env python
 
-import sys, os, re, subprocess
+import sys
+import os
+import re
+import subprocess
+
 
 def print_usage():
-  print >>sys.stderr, "Usage: %s release|devbuild SIGNING_KEY_FILE" % (os.path.basename(sys.argv[0]))
+    print >>sys.stderr, "Usage: %s release|devbuild SIGNING_KEY_FILE" % (os.path.basename(sys.argv[0]))
 
 if len(sys.argv) < 3:
-  print_usage()
-  sys.exit(1)
+    print_usage()
+    sys.exit(1)
 
 basedir = os.path.dirname(os.path.abspath(sys.argv[0]))
 build_type = sys.argv[1]
 
 if not build_type in ["release", "devbuild"]:
-  print_usage()
-  sys.exit(2)
+    print_usage()
+    sys.exit(2)
 
 key = sys.argv[2]
 
+
 def sign_command(*argv):
-  return [
-    "signtool.exe",
-    "sign", "/v",
-    "/d", "Adblock Plus",
-    "/du", "https://adblockplus.org/",
-    "/f", key,
-    "/tr", "http://www.startssl.com/timestamp"
-  ] + list(argv)
+    return [
+        "signtool.exe",
+        "sign", "/v",
+        "/d", "Adblock Plus",
+        "/du", "https://adblockplus.org/",
+        "/f", key,
+        "/tr", "http://www.startssl.com/timestamp"
+    ] + list(argv)
+
 
 def sign(*argv):
-  subprocess.check_call(sign_command(*argv))
+    subprocess.check_call(sign_command(*argv))
+
 
 def read_macro_value(file, macro):
-  handle = open(file, 'rb')
-  for line in handle:
-    match = re.search(r"^\s*#define\s+%s\s+\w?\"(.*?)\"" % macro, line)
-    if match:
-      return match.group(1)
-  raise Exception("Macro %s not found in file %s" % (macro, file))
+    handle = open(file, 'rb')
+    for line in handle:
+        match = re.search(r"^\s*#define\s+%s\s+\w?\"(.*?)\"" % macro, line)
+        if match:
+            return match.group(1)
+    raise Exception("Macro %s not found in file %s" % (macro, file))
 
-version = read_macro_value(os.path.join(basedir, "src", "shared", "Version.h"), "IEPLUGIN_VERSION");
+version = read_macro_value(os.path.join(basedir, "src", "shared", "Version.h"), "IEPLUGIN_VERSION")
 
 if build_type == "devbuild":
-  while version.count(".") < 1:
-    version += ".0"
-  buildnum = subprocess.check_output(['hg', 'id', '-R', basedir, '-n'])
-  buildnum = re.sub(r'\D', '', buildnum)
-  version += ".%s" % buildnum
+    while version.count(".") < 1:
+        version += ".0"
+    buildnum = subprocess.check_output(['hg', 'id', '-R', basedir, '-n'])
+    buildnum = re.sub(r'\D', '', buildnum)
+    version += ".%s" % buildnum
 
 subprocess.check_call([os.path.join(basedir, "createsolution.bat"), version, build_type])
 
 for arch in ("ia32", "x64"):
-  subprocess.check_call([
-    "msbuild",
-    os.path.join(basedir, "build", arch, "adblockplus.sln"),
-    "/p:Configuration=Release", "/target:AdblockPlus;AdblockPlusEngine",
-  ])
+    subprocess.check_call([
+        "msbuild",
+        os.path.join(basedir, "build", arch, "adblockplus.sln"),
+        "/p:Configuration=Release", "/target:AdblockPlus;AdblockPlusEngine",
+    ])
 
-  sign(os.path.join(basedir, "build", arch, "Release", "AdblockPlus.dll"),
-      os.path.join(basedir, "build", arch, "Release", "AdblockPlusEngine.exe"))
+    sign(os.path.join(basedir, "build", arch, "Release", "AdblockPlus.dll"),
+         os.path.join(basedir, "build", arch, "Release", "AdblockPlusEngine.exe"))
 
 installerParams = os.environ.copy()
 installerParams["VERSION"] = version
 subprocess.check_call(["nmake", "/A", "ia32", "x64"], env=installerParams, cwd=os.path.join(basedir, "installer"))
 sign(os.path.join(basedir, "installer", "build", "ia32", "adblockplusie-%s-multilanguage-ia32.msi" % version),
-    os.path.join(basedir, "installer", "build", "x64", "adblockplusie-%s-multilanguage-x64.msi" % version))
+     os.path.join(basedir, "installer", "build", "x64", "adblockplusie-%s-multilanguage-x64.msi" % version))
 
 # If this fails, please check if InnoSetup is installed and added to you PATH
 signparam = " ".join(map(lambda p: "$q%s$q" % p if " " in p else p, sign_command("$f")))
--- import_locales.py
+++ import_locales.py
@@ -18,60 +18,65 @@ gecko_locale_mapping = dict(zip(localeTools.langMappingGecko.values(),
 gecko_locale_mapping["en"] = "en-US"
 
 strings_to_import = {
-  "firstRun.properties/firstRun_acceptableAdsHeadline": "first-run/first-run-aa-title",
-  "firstRun.properties/firstRun_acceptableAdsExplanation": "first-run/first-run-aa-text",
-  "filters.dtd/acceptableAds2.label": "settings/settings-acceptable-ads"
+    "firstRun.properties/firstRun_acceptableAdsHeadline": "first-run/first-run-aa-title",
+    "firstRun.properties/firstRun_acceptableAdsExplanation": "first-run/first-run-aa-text",
+    "filters.dtd/acceptableAds2.label": "settings/settings-acceptable-ads"
 }
 
+
 def read_gecko_locale_strings(locale):
-  locale_strings = {}
-  locale_files = set([key.split("/")[0] for key in strings_to_import.keys()])
-  for locale_file in locale_files:
-    locale_file_path = os.path.join(gecko_locale_base_path, locale, locale_file)
-    if os.path.exists(locale_file_path):
-      locale_strings[locale_file] = localeTools.readFile(locale_file_path)
-    else:
-      locale_strings[locale_file] = {}
-  return locale_strings
+    locale_strings = {}
+    locale_files = set([key.split("/")[0] for key in strings_to_import.keys()])
+    for locale_file in locale_files:
+        locale_file_path = os.path.join(gecko_locale_base_path, locale, locale_file)
+        if os.path.exists(locale_file_path):
+            locale_strings[locale_file] = localeTools.readFile(locale_file_path)
+        else:
+            locale_strings[locale_file] = {}
+    return locale_strings
 
 # This is to keep the locale file format largely intact -
 # SafeConfigParser.write() puts spaces around equal signs.
+
+
 def write_ini(config, file):
-  for index, section in enumerate(config.sections()):
-    if index > 0:
-      file.write("\n")
-    file.write("[%s]\n" % section)
-    items = config.items(section)
-    for key, value in items:
-      file.write("%s=%s\n" % (key, re.sub(r"\s+", " ", value, flags=re.S)))
+    for index, section in enumerate(config.sections()):
+        if index > 0:
+            file.write("\n")
+        file.write("[%s]\n" % section)
+        items = config.items(section)
+        for key, value in items:
+            file.write("%s=%s\n" % (key, re.sub(r"\s+", " ", value, flags=re.S)))
+
 
 def import_locale(ie_locale):
-  gecko_locale = gecko_locale_mapping.get(ie_locale, ie_locale)
-  gecko_locale_strings = read_gecko_locale_strings(gecko_locale)
+    gecko_locale = gecko_locale_mapping.get(ie_locale, ie_locale)
+    gecko_locale_strings = read_gecko_locale_strings(gecko_locale)
 
-  ie_locale_path = "locales/%s.ini" % ie_locale
-  config = SafeConfigParser()
-  config.optionxform = str
-  with codecs.open(ie_locale_path, "r", "utf-8") as ie_locale_file:
-    config.readfp(ie_locale_file)
+    ie_locale_path = "locales/%s.ini" % ie_locale
+    config = SafeConfigParser()
+    config.optionxform = str
+    with codecs.open(ie_locale_path, "r", "utf-8") as ie_locale_file:
+        config.readfp(ie_locale_file)
 
-  for source, target in strings_to_import.iteritems():
-    source_section, source_key = source.split("/")
-    target_section, target_key = target.split("/")
-    if source_key in gecko_locale_strings[source_section]:
-      value = gecko_locale_strings[source_section][source_key]
-      value = re.sub(r"\s*\(&.\)$", "", value).replace("&", "")
-      config.set(target_section, target_key, value)
+    for source, target in strings_to_import.iteritems():
+        source_section, source_key = source.split("/")
+        target_section, target_key = target.split("/")
+        if source_key in gecko_locale_strings[source_section]:
+            value = gecko_locale_strings[source_section][source_key]
+            value = re.sub(r"\s*\(&.\)$", "", value).replace("&", "")
+            config.set(target_section, target_key, value)
 
-  with codecs.open(ie_locale_path, "w", "utf-8") as ie_locale_file:
-    write_ini(config, ie_locale_file)
+    with codecs.open(ie_locale_path, "w", "utf-8") as ie_locale_file:
+        write_ini(config, ie_locale_file)
+
 
 def import_locales():
-  ie_locales = [os.path.splitext(file)[0]
-                for file in os.listdir(ie_locale_base_path)
-                if os.path.isfile(os.path.join(ie_locale_base_path, file))]
-  for ie_locale in ie_locales:
-    import_locale(ie_locale)
+    ie_locales = [os.path.splitext(file)[0]
+                  for file in os.listdir(ie_locale_base_path)
+                  if os.path.isfile(os.path.join(ie_locale_base_path, file))]
+    for ie_locale in ie_locales:
+        import_locale(ie_locale)
 
 if __name__ == "__main__":
-  import_locales()
+    import_locales()
